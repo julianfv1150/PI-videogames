aebd8f5fbfd8814ce146acccd7da88a8e083621e
unknown <julian.f.vega@gmail.com>
2024-05-04 14:26:50 -0300

    DEPLOY change route

--- client/src/utils/login.js
+++ client/src/utils/login.js
@@ -6,6 +6,7 @@ const login = async (userData) => {
     const { email, pass } = userData;
     try {
         const { data }  = await axios.get(`${URL}/users?email=${email}&pass=${pass}`);
+        console.log(data);
         return data;
     } catch (error) {
         return error.response.data;
